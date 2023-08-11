9e15ca3a9592e733a23c9ec8e7ec606ccc65dcb2
Minsu Kim <kms0219kms@naver.com>
2023-08-11 17:16:51 +0700

    :bug: fix: TypeError: Cannot read properties of null (reading 'style')

--- static/assets/js/viewer.js
+++ static/assets/js/viewer.js
@@ -7,16 +7,16 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 window.addEventListener('load', () => {
-	setTimeout(() => {
+	setTimeout(function injectCustomComponents(retry = 0) {
 		let { ticketUrl } = Object.fromEntries(new URLSearchParams(parent.location.search))
-
+	
 		try {
 			/* 상단 헤더 수정 */
 			document.querySelector('discord-header').style.gap = '1.5rem'
 			document.querySelector('.discord-header-icon').style.width = 'unset'
 			document.querySelector('.discord-header-icon > img').style.height = '50px'
 			document.querySelector('.discord-header-text').lastChild.remove()
-
+	
 			/* 하단 푸터 삭제 */
 			for (let i in document.querySelectorAll('div a')) {
 				if (document.querySelectorAll('div a')[i].innerText) {
@@ -24,10 +24,10 @@ window.addEventListener('load', () => {
 					break
 				}
 			}
-
+	
 			/* 다운로드 버튼 추가 */
 			let buttonParent = document.querySelector('.discord-action-row.hydrated')
-
+	
 			let downloadBtn = document.createElement('discord-button')
 			downloadBtn.type = 'success'
 			downloadBtn.emoji = 'https://cdn.discordapp.com/attachments/740171137203568643/1064784853402390538/download_light.png?size=36'
@@ -36,37 +36,43 @@ window.addEventListener('load', () => {
 			downloadBtn.addEventListener('click', () => {
 				window.open(ticketUrl)
 			})
-
+	
 			while (buttonParent.hasChildNodes()) {
 				buttonParent.removeChild(buttonParent.firstChild)
 			}
-
+	
 			document.querySelector('.discord-action-row.hydrated').appendChild(downloadBtn)
 		} catch (e) {
-			/* JS 진행 중 오류 발생 시 오류 페이지로 이동 */
-
+			/* 아직 티켓 파일이 완전히 로드되지 못한 경우 */
+			if (e instanceof TypeError && retry <= 10) {
+				injectCustomComponents(retry + 1)
+				return
+			}
+			
+			/* JS 진행 중 오류 발생 시 오류 페이지로 이동 (로딩 지연 문제 아님) */
+	
 			let errorForm = {
 				form: document.createElement('form'),
 				input: [document.createElement('input'), document.createElement('input')]
 			}
-
+	
 			errorForm.form.method = 'post'
 			errorForm.form.action = '/error'
-
+	
 			errorForm.input[0].name = 'message'
 			errorForm.input[0].value = '웹 티켓을 초기화 하는 중 오류가 발생했어요.'
-
+	
 			errorForm.input[1].name = 'description'
 			errorForm.input[1].value = `아래 오류 정보와 함께 관리자에게 문의해 주세요.<br /><br /><code>${e}</code>`
-
+	
 			document.body.appendChild(errorForm.form)
-
+	
 			errorForm.input.map((x) => {
 				errorForm.form.appendChild(x)
 			})
-
+	
 			window.changeTitle('웹 티켓을 초기화 하는 중 오류가 발생했어요.')
 			errorForm.form.submit()
 		}
-	}, 100)
+	})
 })
